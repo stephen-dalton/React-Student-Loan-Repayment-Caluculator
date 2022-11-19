77d328f32b8ae41f5e79c58129abbbcf62d03cab
Stephen Dalton <daltsdev@icloud.com>
2022-11-19 21:10:36 +0000

    feat: :sparkles: Add postgraduate overseas schema and resolvers

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,8 +1,11 @@
 // import { countryCol } from "../database.js";
-import { getOverSeasThresholdData } from "../functions/getUndergraduateThresholds.js";
+import { getUndergraduateOverSeasThresholdData } from "../functions/getUndergraduateThresholds.js";
+import { getPostGradOverSeasThresholdData } from "../functions/getPostgraduateThresholds.js";
+import { countryCol } from "../database.js";
 async function getCountry(args) {
   try {
     const countryToFind = await countryCol.findOne({ country: args.country });
+    console.log(countryToFind);
     if (!countryToFind) {
       return new Error(
         `${args.country} was not found. Please try a different country`
@@ -17,7 +20,7 @@ async function getCountry(args) {
 
 async function getAllCountries() {
   try {
-    const allCountries = await getOverSeasThresholdData();
+    const allCountries = await getUndergraduateOverSeasThresholdData();
     if (!allCountries) {
       return new Error("Oops...Something Went Wrong. Please Try Again");
     }
@@ -28,4 +31,19 @@ async function getAllCountries() {
   }
 }
 
-export { getCountry, getAllCountries };
+async function getPGCountry(args) {
+  try {
+    const countryToFind = await getPostGradOverSeasThresholdData(args.country);
+    if (!countryToFind) {
+      return new Error(
+        `${args.country} was not found. Please try a different country`
+      );
+    }
+    return countryToFind;
+  } catch (error) {
+    console.error({ error });
+    return new Error("Oop...Something went wrong please try again.");
+  }
+}
+
+export { getCountry, getAllCountries, getPGCountry };
--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -1,5 +1,5 @@
 import { buildSchema } from "graphql";
-import { getCountry, getAllCountries } from "./resolvers.js";
+import { getCountry, getAllCountries, getPGCountry } from "./resolvers.js";
 
 // Define Custom Types
 
@@ -8,6 +8,7 @@ export const schema = buildSchema(`
   type Query {
     country(country: String!): CountryType
     countries: [CountryType]
+    pgCountry(country: String!): PGCountryType
   }
   type CountryType {
     id: ID
@@ -16,7 +17,15 @@ export const schema = buildSchema(`
     exchange_rate: Float
     lower_earning_threshold: Int
     higher_earning_threshold: Int
-    fixed_monthly_repayment: Int
+    fixed_monthly_repayment: String
+  }
+  type PGCountryType {
+    id: ID
+    country: String
+    currency: String
+    exchange_rate: Float
+    earning_threshold: Int
+    fixed_monthly_repayment: String
   }
 `);
 
@@ -24,4 +33,5 @@ export const schema = buildSchema(`
 export const root = {
   country: getCountry,
   countries: getAllCountries,
+  pgCountry: getPGCountry,
 };
