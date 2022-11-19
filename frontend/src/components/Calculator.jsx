0234c617fe88092339ae245408bd4daf8555e247
Stephen Dalton <daltsdev@icloud.com>
2022-11-19 21:13:23 +0000

    feat: :sparkles: add postgrad query to calculator to query conditionally

--- frontend/src/components/Calculator.jsx
+++ frontend/src/components/Calculator.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useGetPGCountry } from "../api/queries";
 import RepaymentDisplay from "./RepaymentDisplay";
 import SalaryInput from "./SalaryInput";
 
@@ -8,14 +9,25 @@ const DIFF_PERCENT = 0.09;
 function Calculator() {
   const [canNotRepay, setCanNotRepay] = useState(false);
   const [repaymentAmount, setRepaymentAmount] = useState(0);
+  const [selectedCountry, setSelectedCountry] = useState({});
+  const [hasPostgradLoan, setHasPostgradLoan] = useState(false);
+  
+  const { data, error, isFetching, isLoading } = useGetPGCountry(
+    selectedCountry.country,
+    hasPostgradLoan
+  );
+  console.log(data);
 
-  const calculateUGRepaymentAmount = (userSalary, country) => {
+  const calculateUGRepaymentAmount = (userSalary, country, hasPostgradLoan) => {
     setCanNotRepay(false);
     const salaryConvertedToGBP = userSalary * country.exchange_rate;
     if (salaryConvertedToGBP < ANNUAL_SALARY_THRESHOLD) {
       console.log(userSalary);
       setCanNotRepay(true);
       return null;
+    }
+    if (hasPostgradLoan) {
+      console.log(data);
     }
     const salaryDiff = salaryConvertedToGBP - ANNUAL_SALARY_THRESHOLD;
     const monthlyAmount = (salaryDiff * DIFF_PERCENT) / 12;
@@ -24,7 +36,13 @@ function Calculator() {
 
   return (
     <section>
-      <SalaryInput calculateUGRepaymentAmount={calculateUGRepaymentAmount} />
+      <SalaryInput
+        selectedCountry={selectedCountry}
+        calculateUGRepaymentAmount={calculateUGRepaymentAmount}
+        setSelectedCountry={setSelectedCountry}
+        hasPostgradLoan={hasPostgradLoan}
+        setHasPostgradLoan={setHasPostgradLoan}
+      />
       <RepaymentDisplay
         canNotRepay={canNotRepay}
         UGRepaymentAmount={repaymentAmount}
