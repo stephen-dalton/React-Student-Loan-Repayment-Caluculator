1755b6b83a4b8913d9772b5f2d5a611273ec2f89
Stephen Dalton <daltsdev@icloud.com>
2023-02-27 22:36:41 +0000

    refactor: :art: improve component structure

--- frontend/src/components/SalaryInput.jsx
+++ frontend/src/components/SalaryInput.jsx
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
-import { useGetCountries } from "../api/queries";
-export default function SalaryInput({
-  hasPostgradLoan,
-  selectedCountry,
-  setHasPostgradLoan,
-  setSelectedCountry,
-  calculateUGRepaymentAmount,
-}) {
+import React, { useState, useMemo } from "react";
+import { useGetUndergraduteCountries } from "../api/queries";
+export default function SalaryInput({ handleSubmission }) {
+  /**
+   * State Setters
+   */
+  const [userCountry, setUserCountry] = useState({});
   const [userSalary, setUserSalary] = useState(0);
+  /**
+   * React Query
+   */
+  const { data, error, isFetching, isLoading } = useGetUndergraduteCountries();
+  /**
+   * Derived State
+   */
+  const isDataLoading = useMemo(
+    () => !data && !error && isLoading && isFetching,
+    [data, error, isFetching, isLoading]
+  );
+  /**
+   * Event Handlers
+   */
 
-  const { data, error, isFetching, isLoading } = useGetCountries();
-  // console.log(data);
-
-  const handleSalaryInputChange = (e) => {
-    setUserSalary(e.target.value);
-  };
-  const handleSalarySubmission = (e) => {
+  function handleFormSubmission(e) {
     e.preventDefault();
-    calculateUGRepaymentAmount(userSalary, selectedCountry, hasPostgradLoan);
-  };
-  const handlePostgradChange = (e) => {
-    setHasPostgradLoan(!hasPostgradLoan);
-  };
-  const handleCountrySelect = (e) => {
-    const selectedCountry = e.target.value;
-    const country = data.countries.find(
-      ({ country }) => country === selectedCountry
-    );
-    setSelectedCountry(country);
-  };
-
-  if (isFetching || isLoading) {
+    handleSubmission(userSalary, userCountry);
+  }
+  /**
+   * Component States
+   */
+  if (isDataLoading) {
     return (
       <section className="container mx-auto w-11/12 rounded bg-white/50 pb-5 backdrop-blur sm:w-6/12 md:w-8/12 lg:w-6/12">
         <div className="animate-pulse">
@@ -50,29 +48,39 @@ export default function SalaryInput({
 
   return (
     <section className="container mx-auto w-11/12 rounded bg-white/50 py-5 backdrop-blur sm:w-6/12 md:w-8/12 lg:w-6/12">
-      <form onSubmit={handleSalarySubmission}>
+      <form onSubmit={handleFormSubmission}>
         <label
-          htmlFor="countries"
+          htmlFor="ug-countries"
           className="mx-auto block w-11/12 text-base font-bold text-slate-800"
         >
           Choose your location
         </label>
         <select
-          name="countries"
-          id="countries"
+          name="ug-countries"
+          id="ug-countries"
           className="mx-auto block w-11/12 rounded-md"
-          onChange={handleCountrySelect}
+          value={userCountry?.country}
+          required
+          onChange={(event) => {
+            setUserCountry(
+              ...data.undergraduateCountries.filter(
+                (country) => country.country === event.target.value
+              )
+            );
+          }}
         >
-          <option>Select Country</option>
-          {data.countries.map((country) => (
-            <option key={country.id} value={country.country}>
-              {country.country}
-            </option>
-          ))}
+          <option value="">— Select Country —</option>
+          <optgroup label="Countries">
+            {data?.undergraduateCountries?.map((country) => (
+              <option key={country._id} value={country.country}>
+                {country.country}
+              </option>
+            ))}
+          </optgroup>
         </select>
         <label htmlFor="salary-input" className="mx-auto block w-11/12 py-5">
           <span className="mx-auto w-11/12 text-base font-bold text-slate-800">
-            Enter Your Annual Gross Salary in {selectedCountry.currency}
+            Enter Your Annual Gross Salary in {userCountry.currency}
           </span>
           <input
             type="number"
@@ -80,15 +88,17 @@ export default function SalaryInput({
             required
             className=" focus:ring- focus:ring-opacity-2 mx-auto mt-1 block w-full appearance-none  rounded-md border-gray-300 shadow-sm focus:border-slate-800 focus:ring-2"
             value={userSalary}
-            onChange={handleSalaryInputChange}
+            onChange={(event) => {
+              setUserSalary(event.target.valueAsNumber);
+            }}
           />
         </label>
         <div className="mx-auto flex w-11/12 items-center pb-5">
           <input
             type="checkbox"
             id="postgraduate"
-            checked={hasPostgradLoan}
-            onChange={handlePostgradChange}
+            // checked={hasPostgradLoan}
+            // onChange={handlePostgradChange}
             className="h-5 w-5 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500"
           />
           <label
