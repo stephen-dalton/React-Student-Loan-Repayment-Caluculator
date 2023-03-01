10ed71b2faa84b7f39eaac6cee8dfb0d31662e77
Stephen Dalton <47178288+stephen-dalton@users.noreply.github.com>
2023-03-01 20:49:58 +0000

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const URI = process.env.MONGO_URI;
-const client = new MongoClient('mongodb+srv://admin:UT3YIyEiI09KJnHz@sl-db.i0hjofa.mongodb.net/slrc?retryWrites=true&w=majority');
+const client = new MongoClient('');
 const studentLoanDatabse = client.db("slrc")
 
 export { studentLoanDatabse };
